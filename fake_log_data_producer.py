be3733dabde992f0ab5e79658ab2929c431467b4
Christopher Sharkey <ev2900@gmail.com>
2022-02-27 20:59:05 -0500

    updated the fake log data producer

--- fake_log_data_producer.py
+++ fake_log_data_producer.py
@@ -9,6 +9,17 @@ import requests
 import json
 import random
 
+from datetime import datetime
+from random import randrange
+from datetime import timedelta
+
+# Define functions to be used later in the code ...
+def random_date(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
 # --------------
 # Step 1
 # --------------
@@ -59,6 +70,9 @@ ip_address_array = ['52.95.4.6', '52.95.8.11', '52.95.6.2', '52.95.1.3', '52.95.
 application_id_array = [1, 2, 3, 4, 5]
 log_message_array = ['Error out of memory', 'Warning memory utilization above 80%', 'Warning CPU utilization above 80%', 'Warning CPU utilization above 50%', 'Warning remaining disk space 20%']
 
+start = datetime.strptime('1/1/2008 12:00 AM', '%m/%d/%Y %I:%M %p')
+end = datetime.strptime('12/31/2020 12:00 PM', '%m/%d/%Y %I:%M %p')
+
 number_of_log_messages_to_send = 1000
 counter = 0
 
@@ -67,7 +81,7 @@ while counter < number_of_log_messages_to_send:
     ip_id_index = random.randint(0,4)
 
     insert_document_r_body = {
-        "eventtime": "2022-02-25 01:00:00",
+        "eventtime": str(random_date(start, end).strftime('%Y-%m-%d %I:%M:%S')),
         "ip-address": ip_address_array[ip_id_index],
         "application-id": application_id_array[ip_id_index],
         "log-message": log_message_array[random.randint(0,4)]
@@ -75,7 +89,9 @@ while counter < number_of_log_messages_to_send:
     
     insert_document_r = requests.put(os_url + '/log-data-1/_doc/' + str(counter+1), auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(insert_document_r_body))
     
-    print('Message # ' + str(counter+1) + insert_document_r.text)
+    print('Message #' + str(counter+1) + ' | ' + insert_document_r.text)
+    
+    # print(json.dumps(insert_document_r_body))
     
     counter = counter + 1
 
