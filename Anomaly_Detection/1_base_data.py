a4a13c63efcd6544bdcc41b0d2d8fc2a0d4c45d3
Christopher Sharkey <ev2900@gmail.com>
2022-04-07 20:00:18 -0400

    updates to the repo

--- Anomaly_Detection/1_base_data.py
+++ Anomaly_Detection/1_base_data.py
@@ -22,9 +22,9 @@ def random_date(start, end):
     return start + timedelta(seconds=random_second)
 
 # --------------
-# Step 1
+# Step 1 - Update this URL with your domain endpoint
 # --------------
-os_url = 'https://search-workshop-domain-rasyvcadldo6spty6dwhtsaw6y.us-east-1.es.amazonaws.com'
+os_url = 'https://search-workshop-domain-atyey3n6ezpbugcxlk623l5ayi.us-east-1.es.amazonaws.com'
 
 # --------------
 # Step 2
@@ -43,7 +43,7 @@ create_index_r_body = {
         "format": "yyyy-MM-dd hh:mm:ss"
       },
       "application_id": {
-        "type": "text"
+        "type": "keyword"
       },
       "cpu_util": {
         "type": "integer"
@@ -60,7 +60,7 @@ create_index_r_body = {
 
 create_index_r = requests.put(os_url + '/infa-logs-1', auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(create_index_r_body))
 
-print('------ Create an index on os cluster ------')
+print('------ Created an index on os cluster ------')
 print(create_index_r.text)
 print('------')
 
@@ -80,7 +80,7 @@ while counter < number_of_log_messages_to_send:
     
     insert_document_r_body = {
         "eventtime": str(random_date(start, end).strftime('%Y-%m-%d %I:%M:%S')),
-        "application_id": 2,
+        "application_id": 'Application ' + str(random.randint(1,10)),
         "cpu_util": random.randint(1,25),
         "memory_util": random.randint(1,25),
         "disk_util": random.randint(1,25)
---
+++ Anomaly_Detection/3_anomoly_detector.py
@@ -0,0 +1,89 @@
+# --------------
+# Prerequisite
+# --------------
+# If you have not already installed the requests package and/or the json package 
+# a. pip install requests
+
+import requests
+
+from datetime import datetime
+from random import randrange
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+# Define functions to be used later in the code ...
+def random_date(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+# --------------
+# Step 1 - Update this URL with your domain endpoint
+# --------------
+os_url = 'https://search-workshop-domain-atyey3n6ezpbugcxlk623l5ayi.us-east-1.es.amazonaws.com'
+
+# --------------
+# Step 2 - Create anomoly detector
+# --------------
+create_detector_body = {
+  "name": "high-cpu",
+  "description": "High CPU Anomaly Detector",
+  "time_field": "eventtime",
+  "indices": [
+    "infa-logs*"
+  ],
+  "feature_attributes": [
+    {
+      "feature_name": "test",
+      "feature_enabled": True,
+      "aggregation_query": {
+        "test": {
+          "sum": {
+            "field": "cpu_util"
+          }
+        }
+      }
+    }
+  ],
+  "detection_interval": {
+    "period": {
+      "interval": 1440,
+      "unit": "Minutes"
+    }
+  },
+  "window_delay": {
+    "period": {
+      "interval": 1,
+      "unit": "Minutes"
+    }
+  },
+  "category_field": [
+    "application_id"
+  ]
+}
+
+create_detector_r = requests.put(os_url + '/infa-logs-1', auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(create_detector_body))
+
+print('------ Created an anomoly detector ------')
+print(create_detector_r.text)
+print('------')
+
+# --------------
+# Step 3 - Train anomoly detector
+# --------------
+
+# the code below does not work. I need to finish this
+
+train_body = {
+  "start_time": 1633048868000,
+  "end_time": 1633394468000
+}
+
+
+
+POST _plugins/_anomaly_detection/detectors/AVDDBYABJkiJN-Gi3zx7/_start
+{
+  "start_time": 1633048868000,
+  "end_time": 1633394468000
+}
