132ab0960b73cb29d13abc011adfd115c2ee3d84
Christopher Sharkey <ev2900@gmail.com>
2022-05-12 20:47:29 -0400

    Update map_IAM_user.py

--- Map_IAM_User/map_IAM_user.py
+++ Map_IAM_User/map_IAM_user.py
@@ -23,8 +23,7 @@ iam_arn = 'arn:aws:iam::376390845435:user/Fluentd_User_OpenSearch'
 # Step 3 - Map IAM user ARN to all_access role
 # --------------
 request_body = {
-	"backend_roles" : [ "all_access"],
-	"users" : [ '"' + iam_arn + '"', "OSMasterUser"]
+	"users" : [iam_arn, "OSMasterUser"]
 }	
 
 map_user_r = requests.put(os_url + '/_plugins/_security/api/rolesmapping/all_access', auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(request_body))
